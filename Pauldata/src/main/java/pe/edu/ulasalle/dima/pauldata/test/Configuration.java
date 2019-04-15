fb7e45f70f4efca511e143e1fa652305ace04b93
yzxyx <jloarteb@ulasalle.edu.pe>
2019-04-14 20:54:51 -0500

    danny gay

---
+++ Pauldata/src/main/java/pe/edu/ulasalle/dima/pauldata/test/Configuration.java
@@ -0,0 +1,9 @@
+package pe.edu.ulasalle.dima.pauldata.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/service/*")
+public class Configuration extends Application{
+
+}
--- Pauldata/src/main/java/pe/edu/ulasalle/dima/pauldata/test/ttsMain.java
+++ Pauldata/src/main/java/pe/edu/ulasalle/dima/pauldata/test/ttsMain.java
@@ -1,9 +1,28 @@
 package pe.edu.ulasalle.dima.pauldata.test;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import pe.edu.ulasalle.dima.pauldata.tts.library.TtsSpanishDefault;
 
+@Path("/tts")
 public class ttsMain {
+	/*
 	TtsSpanishDefault obj = new TtsSpanishDefault();
-	byte[] b = obj.mp3("Ni mierda");
+	byte[] b = obj.mp3("Ni mierda");*/
+	
+	@POST
+	@Path("/{texto}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response talk(TtsSpanishDefault ttsspanishdefault, @PathParam("texto") String texto) {
+		ttsspanishdefault.mp3(texto);
+		return Response.status(Response.Status.CREATED).entity(ttsspanishdefault).build();
+	}
 
 }
